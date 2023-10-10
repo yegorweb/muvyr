9d88ae47e2f2e00f60af4296f124b18b4edf0222
SaveliyShutov <savelijsutov@gmail.com>
2023-10-11 00:08:30 +0400

    milk card

--- src/views/Milk.vue
+++ src/views/Milk.vue
@@ -4,6 +4,63 @@
 
 <template>
   <v-container>
-    Молочная продукция
+    <v-row>
+      <v-col>
+        <v-card height="350px" class="d-flex justify-center">
+          <v-row class="d-flex justify-center">
+            <v-col cols="10">
+              <div class='slash text-center'>
+                <h3>Молоко</h3>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card height="350px" class="d-flex justify-center">
+          <v-row class="d-flex justify-center">
+            <v-col cols="10">
+              <div class='slash text-center'>
+                <h3>Молоко</h3>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card height="350px" class="d-flex justify-center">
+          <v-row class="d-flex justify-center">
+            <v-col cols="10">
+              <div class='slash text-center'>
+                <h3>Молоко</h3>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card height="350px" class="d-flex justify-center">
+          <v-row class="d-flex justify-center">
+            <v-col cols="10">
+              <div class='slash text-center'>
+                <h3>Молоко</h3>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+<style>
+.v-card {
+  border-radius: 12px !important;
+}
+
+.slash {
+  margin-top: 15px;
+  padding: 5px;
+  background-color: pink;
+  border-radius: 8px;
+}
+</style>
