ddd9741c3102bd92f5e24eeaf7669763df765ee2
SaveliyShutov <savelijsutov@gmail.com>
2023-10-10 21:46:47 +0400

    add navbar

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,13 +1,29 @@
 <script setup>
+import { ref } from "vue";
+let drawer = ref(false)
 </script>
 <template>
   <v-app>
     <v-app-bar flat>
-      <v-app-bar-title>
-        Деревня Мувыр
-      </v-app-bar-title>
+      <v-container>
+        <v-row>
+          <v-app-bar-title>
+            Деревня Мувыр
+          </v-app-bar-title>
+          <v-icon class="d-none d-sm-block ma-2" icon="mdi-menu" @click.stop="drawer = !drawer"></v-icon>
+        </v-row>
+      </v-container>
     </v-app-bar>
-
+    <v-navigation-drawer v-model="drawer" location="right" temporary elevation="0">
+      <v-list nav>
+        <v-list-item class="cursor-pointer" prepend-icon="mdi-post" to="/" exact :ripple="false">
+          Главная
+        </v-list-item>
+        <v-list-item class="cursor-pointer" prepend-icon="mdi-post" to="/excursions" exact :ripple="false">
+          Экскурсии
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view />
     </v-main>
