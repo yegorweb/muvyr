46cdc73e57cf9eea45a24ac4907629653811c702
JetGreen <grishadzyin@gmail.com>
2023-10-10 23:51:47 +0400

    add cropper

--- src/views/Admin/PropertyForm.vue
+++ src/views/Admin/PropertyForm.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import ImageCropper from '../../components/ImageCropper.vue'
 
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
@@ -23,6 +24,31 @@ const options = reactive({
         },
     },
 })
+
+let visibleCropperModal = ref(false)
+let previews = ref([])
+let blobImages = []
+
+function deletePreview(preview) {
+    let i = previews.value.indexOf(preview)
+    if (i != -1) {
+        previews.value.splice(i, 1)
+        blobImages.splice(i, 1)
+    }
+}
+
+function addPreview(blob) {
+    visibleCropperModal.value = false;
+    blobImages.push(blob);
+
+    let reader = new FileReader();
+
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+        let base64data = reader.result;
+        previews.value.push(base64data);
+    };
+}
 
 let form = reactive({
     title: '',
@@ -61,16 +87,41 @@ let form = reactive({
                 Количество человек
                 <v-text-field v-model="form.peopleCount"></v-text-field>
             </v-col>
-        </v-row>
-        <v-row class="d-flex">
             <v-col cols="6">
                 Цена
                 <v-text-field v-model="form.price"></v-text-field>
             </v-col>
         </v-row>
-        <v-row>
-            <v-col cols="12">
+        <v-row class="d-flex">
+            <v-col cols="12" md="4">
+                <v-btn> добавить афишу<span>*</span>
+                    <v-dialog v-model="visibleCropperModal" activator="parent">
+                        <v-row class="justify-center">
+                            <v-col cols="12" md="8" lg="6">
+                                <v-card class="pa-4 rounded-lg">
+                                    <ImageCropper @addImage="addPreview" />
 
+                                    <v-card-actions>
+                                        <v-btn @click="visibleCropperModal = false" color="error" class="ml-auto">
+                                            закрыть
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-dialog>
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-row v-for="preview in previews" class="d-flex">
+            <v-col class="d-flex" cols="12" sm="6" md="4">
+                <v-img :src="preview" width="200" class="my-2">
+                    <v-overlay :open-on-click="true" contained class="align-center justify-center" activator="parent">
+                        <v-btn color="error" @click="deletePreview(preview)" icon>
+                            <span class="mdi mdi-delete"></span>
+                        </v-btn>
+                    </v-overlay>
+                </v-img>
             </v-col>
         </v-row>
     </v-container>
