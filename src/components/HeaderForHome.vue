4755623210adce3d0e0831f1f524f10f87f7963c
qbclub <17926057+qbclub@users.noreply.github.com>
2023-10-10 23:29:38 +0400

    home page version 1

---
+++ src/components/HeaderForHome.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="d-flex w-100 justify-space-between align-center">
+        <div class="d-flex align-center">
+            <img src="@/assets/logo.svg" alt="">
+            
+            <span class="logo-text">МУВЫР</span>
+        </div>
+        <v-icon size="large" class="text-white">mdi-menu</v-icon>
+    </div>
+</template>
+
+<style>
+.logo-text {
+    font-family: Marmelad;
+    color: #FFFFFF;
+    margin-left: 18px;
+    font-size: 26px;
+}
+</style>
--- src/views/Muvyr.vue
+++ src/views/Muvyr.vue
@@ -1,12 +1,82 @@
 <script setup>
-
+import HeaderForHome from '@/components/HeaderForHome.vue'
 </script>
 
 <template>
   <div class="wrapper">
+    <video autoplay muted loop id="myVideo">
+      <source src="https://storage.yandexcloud.net/goroda-img/temp-plakat-city/video.mp4" type="video/mp4">
+      Your browser does not support HTML5 video.
+    </video>
 
+    <v-container style="height: 100vh !important; position: relative; bottom: 0; left: 0; top: 0; right: 0;">
+      <div class="w-100 h-100 d-flex flex-column justify-space-between align-start">
+        <HeaderForHome />
+          
+        <div class="w-100">
+          <v-row class="justify-center">
+            <v-col cols="auto">
+              <div class="gray-container title">
+                Возрождённая деревня
+              </div>
+            </v-col>
+          </v-row>
+        </div>
+
+        <div class="list gray-container d-flex flex-column">
+          <div>Дер. МУВЫР</div>
+          <div>МОЛОКО</div>
+          <div>ПРОКАТ</div>
+          <div>СВАДЬБЫ</div>
+          <div>ЭКСКУРСИИ</div>
+          <div>МЕРОПРИЯТИЯ</div>
+          <div>БРОНИРОВАНИЕ</div>
+        </div>
+      </div>
+    </v-container>
   </div>
-  <v-container>
-    Главная страница
-  </v-container>
 </template>
+https://storage.yandexcloud.net/goroda-img/temp-plakat-city/video.mp4
+<style lang="scss">
+.wrapper {
+  min-height: 100dvh;
+  min-width: 100vw;
+  background: #1d421e;
+  position: relative;
+}
+.list {
+  margin-bottom: 20px;
+}
+.list > * {
+  color: #fff;
+  font-family: 'Marmelad';
+  font-size: clamp(1rem, -0.0294rem + 2.9412vw, 1.625rem);
+  cursor: pointer;
+  line-height: 1.5;
+  transition: all .15s;
+
+  &:hover {
+    color: #96c097;
+  }
+}
+.gray-container {
+  padding: 16px;
+  background: rgba($color: #000000, $alpha: 0.35);
+  border-radius: 12px;
+}
+.title {
+  padding: 16px;
+  line-height: 0.85;
+  font-family: 'Marmelad', cursive;
+  font-size: clamp(1.75rem, -3.6029rem + 15.2941vw, 5rem);
+  color: #FFFFFF;
+  text-align: center;
+}
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%; 
+  min-height: 100%;
+}
+</style>
