e2aabeee58096797123e21cd9df73ca157a12198
qbclub <17926057+qbclub@users.noreply.github.com>
2023-10-11 05:58:11 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
