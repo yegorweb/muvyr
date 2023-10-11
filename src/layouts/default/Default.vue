163e7bf3bc650f7ef65ecac3146ffe8f8c1e3289
qbclub <17926057+qbclub@users.noreply.github.com>
2023-10-12 00:12:35 +0400

    added padding bottom

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -7,7 +7,7 @@ import Header from '@/components/Header.vue';
   <v-app>
     <Header />
 
-    <v-main>
+    <v-main style="padding-bottom: 80px;">
       <router-view v-slot="{ Component }">
         <transition name="fade">
           <component :is="Component" />
