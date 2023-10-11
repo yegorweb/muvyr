7a3f5eaa61a7156e0632713dc9730b1cac16f7ba
qbclub <17926057+qbclub@users.noreply.github.com>
2023-10-11 05:57:27 +0400

    production

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,42 @@ export default defineConfig({
         }],
       },
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'images/apple-touch-icon.png'],
+      manifest: {
+        name: 'Мувыр',
+        short_name: 'Мувыр',
+        description: 'Мувыр - возрождение',
+        theme_color: '#ffffff',
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
