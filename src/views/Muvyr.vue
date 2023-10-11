eaa9d32ab5ac59e252b3aae590c9bc018d8e361a
Yegor Knyazev <egorknyazev10@gmail.com>
2023-10-11 09:46:47 +0400

    added normal height in wrapper

--- src/views/Muvyr.vue
+++ src/views/Muvyr.vue
@@ -42,8 +42,8 @@ let router = useRouter()
 
 <style lang="scss">
 .wrapper {
-  min-height: 100dvh;
-  min-width: 100vw;
+  width: 100vw;
+  height: calc(100vh - 80px);
   background: #1d421e;
   position: relative;
 }
