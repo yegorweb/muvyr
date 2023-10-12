da1cea0b392c65b0276268e3751838d81f5a7a67
Yegor Knyazev <egorknyazev10@gmail.com>
2023-10-12 11:52:32 +0400

    fixed wedding

--- src/views/Wedding.vue
+++ src/views/Wedding.vue
@@ -40,38 +40,46 @@ let images = ref([{
       <v-col cols="12" class="text-center" style="font-size: 35px;">
         <span style="font-family: 'Dela Gothic One';">Свадьбы</span>
       </v-col>
-      <v-col cols="12" class="d-flex justify-end">
-        <v-btn @click="bookEventDialog = true">Заказать</v-btn>
-        <v-dialog v-model="bookEventDialog" width="1024">
-          <v-card class="pa-4" title="Заказать мероприятие">
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field label="Электроная почта" required></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field label="Мероприятие" required></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6">
-                    <v-text-field label="телефон" required></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6">
-                    <v-text-field label="Дата и время" required></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn variant="text" @click="bookEventDialog = false">
-                Закрыть
-              </v-btn>
-              <v-btn variant="text" @click="bookEventDialog = false">
-                отправить
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+      <v-col cols="12" class="d-flex justify-center">
+        <v-btn variant="outlined" :ripple="false" @click="bookEventDialog = true">Заказать</v-btn>
+
+        <v-dialog v-model="bookEventDialog">
+          <v-container class="d-flex justify-center">
+            <v-col cols="12" md="8" lg="4" class="pa-0">
+              <v-card class="rounded-lg pt-6 pa-4">
+                <h3 class="w-100 text-center">
+                  Заказать мероприятие
+                </h3>
+
+                <v-col>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field hide-details variant="outlined" density="compact" label="Электроная почта" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field hide-details variant="outlined" density="compact" label="Мероприятие" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                      <v-text-field hide-details variant="outlined" density="compact" label="Телефон" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                      <v-text-field hide-details variant="outlined" density="compact" label="Дата и время" required></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn variant="text" @click="bookEventDialog = false">
+                    Закрыть
+                  </v-btn>
+                  <v-btn variant="outlined" @click="bookEventDialog = false">
+                    отправить
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-container>
         </v-dialog>
       </v-col>
     </v-row>
