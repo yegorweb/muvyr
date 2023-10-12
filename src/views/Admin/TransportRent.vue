a5309c1a4eae2ecd5be7fc965464c6a61316cd1d
qbclub <17926057+qbclub@users.noreply.github.com>
2023-10-12 07:32:51 +0400

    added text in transport rent

--- src/views/Admin/TransportRent.vue
+++ src/views/Admin/TransportRent.vue
@@ -30,41 +30,49 @@ let list = ref([{
 },])
 </script>
 <template>
-    <v-table>
-        <thead>
-            <tr>
-                <th class="text-left">
-                    Дата
-                </th>
-                <th class="text-left">
-                    Название обьекта
-                </th>
-                <th class="text-left">
-                    Пользователь
-                </th>
-                <th class="text-left">
-                    Время пользования
-                </th>
-                <th class="text-left">
-                    Итоговая цена
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in list">
-                <td>{{ item.data }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.customer }}</td>
-                <td>{{ item.time }}</td>
-                <td>{{ item.totalPrice }}</td>
-            </tr>
-        </tbody>
-    </v-table>
     <v-row>
-        <v-col cols="12" class="mt-3">
-            <v-btn to="/admin/product-form">
-                создать Объект проката
-            </v-btn>
+        <v-col cols="12">
+            Журнал проката
+        </v-col>
+
+        <v-col cols="12">
+            <v-table>
+                <thead>
+                    <tr>
+                        <th class="text-left">
+                            Дата
+                        </th>
+                        <th class="text-left">
+                            Название обьекта
+                        </th>
+                        <th class="text-left">
+                            Пользователь
+                        </th>
+                        <th class="text-left">
+                            Время пользования
+                        </th>
+                        <th class="text-left">
+                            Итоговая цена
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in list">
+                        <td>{{ item.data }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.customer }}</td>
+                        <td>{{ item.time }}</td>
+                        <td>{{ item.totalPrice }}</td>
+                    </tr>
+                </tbody>
+            </v-table>
+            <v-row>
+                <v-col cols="12" class="mt-3">
+                    <v-btn to="/admin/product-form">
+                        создать Объект проката
+                    </v-btn>
+                </v-col>
+            </v-row>
         </v-col>
     </v-row>
 </template>
